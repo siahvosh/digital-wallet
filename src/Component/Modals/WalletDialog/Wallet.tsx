4ccd0ee83c983922c7f0db5e0e7d72bd17b684dd
siavash <siablack370@gmail.com>
2024-11-10 19:05:53 +0330

    set ttile for modal

--- src/Component/Modals/WalletDialog/Wallet.tsx
+++ src/Component/Modals/WalletDialog/Wallet.tsx
@@ -2,16 +2,11 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
 import {useEffect, useState} from "react";
 import {Grid2} from "@mui/material";
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
-import FullscreenIcon from "@mui/icons-material/Fullscreen";
 import './Wallet.css'
 import {useData} from "../../../DataContext";
 
@@ -26,7 +21,6 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 
 
 const Wallet = () => {
-
     const [open, setOpen] = useState<boolean>(false);
 
     const { data } = useData();
@@ -48,7 +42,7 @@ const Wallet = () => {
                 aria-labelledby="customized-dialog-title"
                 open={open}
             >
-                <DialogContent dividers style={{background: '#2bab84'}}>
+                <DialogContent dividers className={'actions-content'}>
                     <Grid2
                         alignContent="center"
                         justifyContent="center"
@@ -60,25 +54,25 @@ const Wallet = () => {
                     >
                         <Grid2 size={12}>
                             <div style={{justifyContent: 'center', display: 'flex'}}>
-                                <span style={{fontSize: '6rem', fontWeight: '600'}}>Inventory</span> <br/>
+                                <span className={'title-style'}>Inventory</span> <br/>
                             </div>
                         </Grid2>
                         <Grid2 size={12} style={{justifyContent: 'center', display: 'flex'}}>
-                            <span style={{fontSize: '2.5rem', fontWeight: '600'}}>20.000.000 Rials</span>
+                            <span className={'inventory-style'}>20.000.000 Rials</span>
                         </Grid2>
                         <Grid2 size={12} style={{justifyContent: 'center', display: 'flex'}}>
                             <IconButton style={{marginTop: '3rem'}} className={'btn'} onClick={handleClose} aria-label="delete" size="small">
-                                <FullscreenIcon/>
+                                <FullscreenExitIcon/>
                             </IconButton>
                         </Grid2>
                     </Grid2>
                 </DialogContent>
-                <DialogContent dividers style={{borderTopRightRadius: '50px !important'}}>
+                <DialogContent dividers>
                    <Grid2 alignContent="center" justifyContent="center" container size={12} spacing={3} style={{height: '100%'}}>
                        <Grid2 size={12}>
                            <div className={'option btn'}>
-                               <span style={{padding: '2rem', fontSize: '2rem'}}>Wallet to Wallet</span><br/>
-                               <span style={{padding: '2rem', fontSize: '1rem'}}>از کیف پول به کیف پول دیگر</span>
+                               <span className={'btn-title'}>Wallet to Wallet</span><br/>
+                               {/*<span style={{padding: '2rem', fontSize: '1rem'}}>از کیف پول به کیف پول دیگر</span>*/}
                            </div>
                        </Grid2>
                        <Grid2 size={12}>
@@ -93,7 +87,6 @@ const Wallet = () => {
                                <span style={{padding: '2rem', fontSize: '1rem'}}>از کیف پول به کیف پول دیگر</span>
                            </div>
                        </Grid2>
-
                    </Grid2>
                 </DialogContent>
             </BootstrapDialog>
