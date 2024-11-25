96091506968edf2bb6d67b0a2cdeb73a7d2f0e33
siavash <siablack370@gmail.com>
2024-11-25 17:36:01 +0330

    add top up wallet page

--- src/Pages/WalletActions/AddMoney.tsx
+++ src/Pages/WalletActions/AddMoney.tsx
@@ -1,6 +1,88 @@
+import * as React from 'react'
+import {Button, Card, Grid2, TextField} from "@mui/material";
+import {BalanceCard} from "../../Component/BalanceCard/BalanceCard";
+import i18n from "../../i18next";
+import {useTranslation} from "react-i18next";
+import {useState} from "react";
+import {Sep1000} from "../../indexHelper";
+
+
 export const AddMoney = () => {
+
+    const { t } = useTranslation('transfer');
+
+    interface priceItemsType {
+        price: string;
+        num: string
+    }
+    const [priceItems] = useState<priceItemsType[]>([
+        {price: '100,000', num: '1'},
+        {price: '200,000', num: '2'},
+        {price: '300,000', num: '3'},
+        {price: '500,000', num: '4'},
+        {price: '1,000,000', num: '5'},
+        {price: '2,000,000', num: '6'},
+    ])
+    const [customPrice, setCustomPrice] = useState<string>('')
+    const [selectedIndex, setSelectedIndex] = useState<number>()
+
+    const handleItemClick = (value: string, index: number) => {
+        setSelectedIndex(index)
+        setCustomPrice(value)
+    };
+    const handelCustomPrice = (value: any) => {
+        Sep1000(value)
+        setCustomPrice(Sep1000(value))
+        setSelectedIndex(-1)
+    }
+
     return(
-        <h1>addMoney</h1>
+        <React.Fragment>
+            <Card style={{height: '100%'}}>
+                <Grid2 size={12} container spacing={0} style={{height: '100%'}}>
+                    <Grid2 container size={12} spacing={1} className={'content'}>
+                        <BalanceCard/>
+                    </Grid2>
+                    <Grid2 container style={{marginTop: '1.5rem'}} size={12} spacing={3} className={'action-container'}>
+                        <Grid2 size={12} style={{justifyContent: 'center', display: 'center'}}>
+                            <div style={{borderRadius: '8px' ,width: '65vw', height: '3rem', justifyContent: 'space-between' ,margin: 'auto', alignContent: 'center'}}>
+                                <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                    <TextField
+                                        sx={{
+                                            '& .MuiInputLabel-root': {
+                                                left: i18n.language === 'fa' ? 'auto' : 0,
+                                            },
+                                        }}
+                                        onChange={(e) => handelCustomPrice(e.target.value)}
+                                        variant={'standard'}
+                                        style={{width: '100%'}} value={customPrice} label={t('transfer.customPrice')}/>
+                                </div>
+                            </div>
+                        </Grid2>
+                        <Grid2 size={12} container style={{justifyContent: 'center', display: 'center', width: '65vw', margin: 'auto', alignContent: 'center'}}>
+                            <Grid2 size={12}>
+                                <span style={{display: 'flex', justifyContent: 'start'}}>{t('transfer.customPriceLabel')}</span>
+                            </Grid2>
+
+                            {priceItems.map((item, index) => (
+                                <Grid2 size={{ xs: 12, md: 4, lg: 4}}>
+                                    <div className={selectedIndex === index ? 'active-items' : 'btn'} onClick={() => handleItemClick(item.price, index)} style={{
+                                        cursor: 'pointer',
+                                        borderRadius: '12px',
+                                        padding: '1rem',
+                                    }}>
+                                        {item.price}
+                                    </div>
+                                </Grid2>
+                            ))}
+                        </Grid2>
+                        <Grid2 size={12}  style={{margin: 'auto', width: '65vw'}}>
+                            <Button style={{width: '100%'}}>{t('transfer.transfer')}</Button>
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
+            </Card>
+        </React.Fragment>
     )
 }
 
