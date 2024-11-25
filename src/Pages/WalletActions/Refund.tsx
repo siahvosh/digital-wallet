4bb401998bd5ff31fd457e6715cb2faf72435f69
siavash <siablack370@gmail.com>
2024-11-25 17:57:37 +0330

    set locles for refund

--- src/Pages/WalletActions/Refund.tsx
+++ src/Pages/WalletActions/Refund.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Card, Grid2, TextField} from "@mui/material";
+import {Button, Card, Grid2, TextField} from "@mui/material";
 import {BalanceCard} from "../../Component/BalanceCard/BalanceCard";
 import i18n from "../../i18next";
 import {useState} from "react";
@@ -10,6 +10,7 @@ export const Refund = () => {
     const [customPrice, setCustomPrice] = useState<string>('')
     const { t } = useTranslation('transfer');
 
+    const [curdNumber, setCurdNumber] = useState<string>('')
 
     const handelCustomPrice = (value: any) => {
         Sep1000(value)
@@ -29,7 +30,13 @@ export const Refund = () => {
                         <span>اطلاعات مورد نظر وارد کنید.</span>
                     </Grid2>
                     <Grid2 size={12} style={{justifyContent: 'center', display: 'center'}}>
-                        <div style={{borderRadius: '8px' ,width: '65vw', height: '3rem', justifyContent: 'space-between' ,margin: 'auto', alignContent: 'center'}}>
+                        <div style={{
+                            borderRadius: '8px',
+                            width: '65vw',
+                            height: '3rem',
+                            justifyContent: 'space-between',
+                            margin: 'auto',
+                            alignContent: 'center'}}>
                             <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                                 <TextField
                                     sx={{
@@ -42,6 +49,30 @@ export const Refund = () => {
                                     style={{width: '100%'}} value={customPrice} label={t('transfer.customPrice')}/>
                             </div>
                         </div>
+                    </Grid2>
+                        <div style={{
+                            width: '65vw',
+                            height: '3rem',
+                            justifyContent: 'space-between',
+                            margin: 'auto',
+                            alignContent: 'center'
+                        }}>
+                            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                <TextField
+                                    sx={{
+                                        '& .MuiInputLabel-root': {
+                                            left: i18n.language === 'fa' ? 'auto' : 0,
+                                        },
+                                    }}
+                                    value={curdNumber}
+                                    onChange={(e) => setCurdNumber(e.target.value)}
+                                    variant={'standard'}
+                                    style={{width: '100%'}}
+                                    label={t('transfer.numberCard')}/>
+                            </div>
+                        </div>
+                    <Grid2 size={12} style={{margin: 'auto', width: '65vw'}}>
+                        <Button style={{width: '100%'}}>{t('refund.refundBtn')}</Button>
                     </Grid2>
                 </Grid2>
             </Card>
