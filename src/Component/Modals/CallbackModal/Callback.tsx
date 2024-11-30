c94c13f6e88cb179391956ab2c5c3034503d2cf7
siavash <siablack370@gmail.com>
2024-11-30 23:32:09 +0330

    map on status obj

--- src/Component/Modals/CallbackModal/Callback.tsx
+++ src/Component/Modals/CallbackModal/Callback.tsx
@@ -1,7 +1,7 @@
 import {Button, Card, CardActions, CardContent, CardMedia} from "@mui/material";
 import {styled} from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
-import {useState} from "react";
+import React, {useState} from "react";
 import imageUrl from "../../../Assets/success.png"
 
 
@@ -21,21 +21,22 @@ export const Callback = () => {
         setOpen(false);
     };
 
+    const [st, setSt] = useState<string>('refund')
+
     interface statusType {
         status: string;
         title: string;
         description: string;
     }
 
-    const status = useState<statusType[]>(
+    const [statusList] = useState<statusType[]>(
         [
-            {status: 'transfer', title: 'انتقال با موفقیت انجام شد\n', description: 'ملبغ 100.000 ریال از کیف شما به حساب 5044456263980238 واریز شد'},
-            {status: 'add', title: 'کیف پول با موفقیت شارژ شد', description: 'کیف پول به مبلغ 100.000 شارژ شد'},
-            {status: 'refund', title: 'برداشت با موفقیت انجام شد', description: 'مبلغ 100.000 از کیف پول شما برداشت شد'},
+            {status: 'transfer', title: 'انتقال با موفقیت انجام شد', description: 'ملبغ 100.000 ریال از کیف شما به حساب 5044456263980238 واریز شد'},
+            {status: 'add', title: 'کیف پول با موفقیت شارژ شد', description: 'کیف پول شما به مبلغ 100.000 ریال شارژ شد'},
+            {status: 'refund', title: 'برداشت با موفقیت انجام شد', description: 'مبلغ 100.000 ریال از کیف پول شما برداشت شد'},
 
         ]
     )
-
 
     return (
         <BootstrapDialog
@@ -44,19 +45,33 @@ export const Callback = () => {
             open={open}
         >
             <Card style={{height: '30rem', width: '30rem'}}>
-                <CardMedia
-                    component="img"
-                    image={imageUrl}
-                    alt="Paella dish"
-                    style={{height: '70%', width: '70%', margin: 'auto'}}
-                />
-                <CardContent>
-                    <span style={{display: 'flex', justifyContent: 'center', fontWeight: '700'}}>انتقال با موفقیت انجام شد</span>
-                    <span style={{display: 'flex', justifyContent: 'center', fontWeight: '300', textAlign: 'center', marginTop: '0.5rem'}}>ملبغ 100.000 ریال از کیف شما به حساب 5044456263980238 واریز شد</span>
-                </CardContent>
-                <CardActions style={{position: 'absolute', bottom: '0', right: '0'}}>
-                    <Button size={'small'} onClick={handleClose} color="primary">متوجه شدم</Button>
-                </CardActions>
+
+                {statusList
+                    .filter(item => item.status === st)
+                    .map((item, index) => (
+                            <React.Fragment key={index}>
+                                <CardMedia
+                                    component="img"
+                                    image={imageUrl}
+                                    alt="Paella dish"
+                                    style={{height: '70%', width: '70%', margin: 'auto'}}
+                                />
+                                <CardContent>
+                                    <span style={{display: 'flex', justifyContent: 'center', fontWeight: '700'}}>{item.title}</span>
+                                    <span style={{
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        fontWeight: '300',
+                                        textAlign: 'center',
+                                        marginTop: '0.5rem'
+                                    }}>{item.description}</span>
+                                </CardContent>
+                                <CardActions style={{position: 'absolute', bottom: '0', right: '0'}}>
+                                    <Button size={'small'} onClick={handleClose} color="primary">متوجه شدم</Button>
+                                </CardActions>
+                            </React.Fragment>
+
+                ))}
             </Card>
 
         </BootstrapDialog>
