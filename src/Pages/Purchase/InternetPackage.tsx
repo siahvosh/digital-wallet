34108acc36dae3a932cbe00b28379ec1d9480480
siavash <siablack370@gmail.com>
2024-12-03 21:50:11 +0330

    set header page

--- src/Pages/Purchase/InternetPackage.tsx
+++ src/Pages/Purchase/InternetPackage.tsx
@@ -1,7 +1,19 @@
+import {Card, Grid2, TextField} from "@mui/material";
+import React, {useState} from "react";
 
 
 export const InternetPackage = () => {
+    const [phoneNumber, setPhoneNumberValue] = useState<string>()
     return (
-        <h1>InternetPackage</h1>
+        <React.Fragment>
+            <Card style={{height: '100%', width: '100%' }}>
+                <Grid2 size={12} container spacing={0} style={{height: '100%'}}>
+                    <Grid2 container size={12} spacing={1} className={'content'} style={{alignItems: 'end'}}>
+                        <span style={{color: 'white',fontSize: '25px', fontWeight: '800', position: 'relative', top: '0', right: '0.4rem'}}>شماره همراه</span>
+                        <TextField onChange={(e) => setPhoneNumberValue(e.target.value)} style={{background: 'white', borderRadius: '12px'}} fullWidth ></TextField>
+                    </Grid2>
+                </Grid2>
+            </Card>
+        </React.Fragment>
     )
 }
