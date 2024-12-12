4c2b9a2066019167d0d188d44c473a076ef9305f
siavash <siablack370@gmail.com>
2024-12-12 20:20:37 +0330

    set navigate for menu icon

--- src/Component/Menu/MenuBar.tsx
+++ src/Component/Menu/MenuBar.tsx
@@ -4,24 +4,28 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
 import LayersIcon from "@mui/icons-material/Layers";
 import HomeIcon from "@mui/icons-material/Home";
+import {useNavigate} from "react-router-dom";
 
 const MenuBar = () => {
+    const navigate = useNavigate();
+
     interface homeBtnType {
         icon: JSX.Element;
         size: { xs: number; md: number };
+        path: string;
     }
     const [homeBtn] = useState<homeBtnType[]>([
-        { icon: <SettingsIcon/>, size: {xs: 6, md: 6}},
-        { icon: <LocalGroceryStoreIcon/>, size: {xs: 6, md: 6}},
-        { icon: <LayersIcon/>, size: {xs: 6, md: 6}},
-        { icon: <HomeIcon/>, size: {xs: 6, md: 6}},
+        { icon: <SettingsIcon/>, size: {xs: 6, md: 6}, path: '/setting'},
+        { icon: <LocalGroceryStoreIcon/>, size: {xs: 6, md: 6}, path: '/setting'},
+        { icon: <LayersIcon/>, size: {xs: 6, md: 6}, path: '/analytics'},
+        { icon: <HomeIcon/>, size: {xs: 6, md: 6}, path: '/'},
 
     ])
     return (
         <Card className={"menu"}>
             <div style={{justifyContent: 'space-between', display: 'flex', padding: '0rem 1rem 1rem 1rem', color: '#2bab84'}}>
                 {homeBtn.map((item, idx: number) => (
-                    <div key={idx}> {item.icon} </div>
+                    <div onClick={() => navigate(item.path)} style={{cursor: 'pointer'}} key={idx}> {item.icon} </div>
                 ))}
             </div>
         </Card>
