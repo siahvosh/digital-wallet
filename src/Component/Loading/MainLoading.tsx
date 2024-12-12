8a29dd15edaf3a3aa821645eba7ea3253c8e1a39
siavash <siablack370@gmail.com>
2024-12-12 17:26:15 +0330

    set loader style

--- src/Component/Loading/MainLoading.tsx
+++ src/Component/Loading/MainLoading.tsx
@@ -6,7 +6,20 @@ export const MainLoading = () => {
             <div className="loader">
 
             </div>
-            <div className={'loader-text'}>Loading</div>
+            <div className={'loader-text'}>
+                <div className="card">
+                    <div className="loaders">
+                        <p className={'text'}>loading</p>
+                        <div className="words">
+                            <span className="word">buttons</span>
+                            <span className="word">forms</span>
+                            <span className="word">switches</span>
+                            <span className="word">cards</span>
+                            <span className="word">buttons</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </>
     )
